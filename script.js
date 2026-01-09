025d14d5128711109b9f8002f8612759264a033c
miguel calle <callerestrepomigueldavid@gmail.com>
2026-01-08 20:50:37 -0500

    test validation

--- script.js
+++ script.js
@@ -32,15 +32,43 @@ btn.addEventListener("click", () => {
     current === "dark" ? "light" : "dark"
     );
 });
+// ---------- GUARDAR TAREA
+function Guardar() {
+  localStorage.setItem('taskTitle', document.getElementById('taskTitle').value);
+  // localStorage.getItem('taskTitle');
 
+  if (validateTaskInput()) {
+    const contenedorPadre = document.querySelector('#contenedor-padre-tareas')
+    const hijoPadre = document.createElement('p')
+
+    hijoPadre.innerHTML = `<div class="col-6 cardCard"> 
+                            <section class="card">
+                                <div class="card-body">
+                                    <header class="d-flex">
+                                        <input class="form-check-input" type="radio" name="radioDefault">
+                                        <h6 class="card-title ms-3">${document.getElementById('taskTitle').value}</h6>
+                                    </header>
+                                    <p class="card-text">Details: ${document.getElementById('taskDescription').value}</p>
+                                    <p class="card-text">Date: ${document.getElementById('taskDueDate').value}</p>
+                                    <p>Priority:<span class="btn badge ms-1 p-1 btn-primary">${document.getElementById('taskPriority').value}</button>
+                                    <p>Category:<span class="btn badge ms-1 p-1 btn-info">${document.getElementById('taskCategory').value}</button>
+                                    <p>Status:<span class="btn badge ms-1 p-1 btn-danger">Pending</class=></span></p>
+                                </div>
+                            </section>
+                        </div>`
+
+    contenedorPadre.append(hijoPadre)
+
+
+  }  
+}
 
 function validateTaskInput() {
     const title = document.getElementById('taskTitle').value;
-    const description = document.getElementById('taskDescription').value;
-    const dueDate = document.getElementById('taskDueDate').value;
-    console.log(title, description, dueDate);
+    // const description = document.getElementById('taskDescription').value;
+    // const dueDate = document.getElementById('taskDueDate').value;
     
-    if( title === '' && description === '' && dueDate === '') {
+    if( title == '' ) {
         alert('Please fill in all required fields: Title, Description, and Due Date.');
         return false;
     }
@@ -48,4 +76,5 @@ function validateTaskInput() {
         alert('Task saved successfully!');
         return true ;
     }
+  
 }
